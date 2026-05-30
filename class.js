2f6e39660b7a96fd24fd25066040725e6dea3d8a
andrew_guk <gukandrey91@gmail.com>
2026-05-30 17:06:03 +0300

    add lesson class

---
+++ class.js
@@ -0,0 +1,87 @@
+
+// function createCar (brand, maxSpeed) {
+//     return {
+//         brand: brand,
+//         maxSpeed: maxSpeed,
+//         startEngine() {
+//             console.log(`${this.brand} engine is started`);
+//         }
+//     }
+// }
+//
+// const bmw = createCar('bmw', 200)
+// const opel = createCar('opel', 150)
+//
+//
+// console.log(opel.startEngine === bmw.startEngine)
+
+// function CarCreator (brand, maxSpeed) {
+//     this.brand = brand
+//     this.maxSpeed = maxSpeed
+//     this.startEngine = function () {
+//         console.log(`${this.brand} engine is started`)
+//     }
+// }
+//
+// CarCreator.prototype.startEngine = function () {
+//     console.log(`${this.brand} engine is started`)
+// }
+//
+// const bmw = new CarCreator('bmw', 200)
+// const opel = new CarCreator('opel', 150)
+//
+//
+// console.log(opel.startEngine === bmw.startEngine)
+
+
+//
+// class Car {
+//     constructor(brand, maxSpeed) {
+//         this.brand = brand
+//         this.maxSpeed = maxSpeed
+//     }
+//
+//     startEngine () {
+//         console.log(`${this.brand} started`)
+//     }
+//     // stopEngine = function () {
+//     //     console.log(`${this.brand} stopped`)
+//     // }
+//
+// }
+//
+// const bmw = new Car('bmw', 200)
+// const opel = new Car('opel', 180)
+// // console.log(opel.stopEngine === bmw.stopEngine)
+
+
+class Car {
+    #wheels = 4
+
+    constructor(brand, maxSpeed) {
+        this.brand = brand
+        this.maxSpeed = maxSpeed
+    }
+
+    startEngine () {
+        console.log(`${this.brand} started`)
+    }
+
+    setWheels (value) {
+        if (value > 5) {
+            throw Error(`Value should be <= 5`)
+        }
+        this.#wheels = value
+    }
+
+    getWheels () {
+        return this.#wheels
+    }
+
+}
+
+const bmw = new Car('bmw', 200)
+console.log(bmw.getWheels())
+bmw.setWheels(5)
+console.log(bmw.getWheels())
+bmw.setWheels(6)
